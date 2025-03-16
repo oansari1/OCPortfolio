587eff917b05360e83e142451d0d875408d84c76
Ozzie Ansari <oansari1@gmail.com>
2025-03-16 14:46:04 -0700

    Fix import case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 import Home from "./pages/Home"
-import About from "./pages/about"
+import About from "./pages/About"
 import Projects from "./pages/Projects"
 import ProjectCategory from "./pages/ProjectCategory"
 import NotFound from "./pages/NotFound"
